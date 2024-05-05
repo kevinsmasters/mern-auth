4c0fa713530126a7821c937d61b548772342d7d2
Kevin Masters <irimiuke@gmail.com>
2024-05-05 10:24:06 -0400

    stub in unit tests for Users

---
+++ backend/testing/user.controller.spec.js
@@ -0,0 +1,38 @@
+describe('Test /', ()=>{
+  describe('register a new user', ()=>{
+    // stub in db connection
+    // it: user already exists
+    // it: invalid user data
+    // it: successful user submission
+    // it: redirects to (?)
+  });
+});
+
+describe('Test /auth', ()=> {
+  describe('login user', ()=> {
+    // db connect
+    // it: user does not exist
+    // it: invalid login credentials
+    // it: successful login
+    // it: redirects to (?)
+  });
+});
+
+describe('Test /logout', ()=> {
+  describe('logout user', ()=> {
+    // it: logs the user out
+    // it: redirects to (?)
+  });
+});
+
+describe('Test /profile', ()=> {
+  describe('list user profiles', ()=> {
+    // it: gets all users
+  });
+  describe('show a specific user profile', ()=> {
+    // it: show user id(x)
+  });
+  describe('update a user', ()=> {
+    // it: changes a user attribute
+  })
+});
