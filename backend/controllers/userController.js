5cfebe9b7feba28c2e94a7cc02a9a397a10e120d
Kevin Masters <irimiuke@gmail.com>
2024-05-05 19:40:55 -0400

    login and set token, logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,25 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 // @desc  Auth user/set token
 // route  POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req,res)=> {
-  res.status(200).json({ message: 'Auth user' });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password')
+  }
 });
 
 // @desc  Register a new user
@@ -16,7 +30,7 @@ const registerUser = asyncHandler(async (req,res)=> {
 
   const userExists = await User.findOne({email});
 
-  if(userExists) {
+  if (userExists) {
     res.status(400);
     throw new Error('User already exists');
   }
@@ -27,7 +41,8 @@ const registerUser = asyncHandler(async (req,res)=> {
     password
   });
 
-  if(user) {
+  if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -43,7 +58,11 @@ const registerUser = asyncHandler(async (req,res)=> {
 // route  POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req,res)=> {
-  res.status(200).json({ message: 'Logout user' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+  res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc  Get user profile
