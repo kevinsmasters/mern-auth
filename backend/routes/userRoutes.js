08695d30f05c9ca272e7e9ad863faa560f367317
Kevin Masters <irimiuke@gmail.com>
2024-05-05 19:59:56 -0400

    adds authentication middleware, restrict routes and update/view functions added in

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,11 +7,12 @@ import {
   getUserProfile,
   UpdateUserProfile
 } from '../controllers/userController.js';
- 
+import { protect } from '../middleware/authMiddleware.js';
+
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(UpdateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, UpdateUserProfile);
 
 
 export default router;
