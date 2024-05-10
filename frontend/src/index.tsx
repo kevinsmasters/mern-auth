721f42333dcf3b117d00d15a923c84c67f513feb
Kevin Masters <irimiuke@gmail.com>
2024-05-09 20:55:26 -0400

    install react-router-dom and start setting up routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import App from './App';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App /> }>
+      <Route index={ true } path='/' element={ <HomeScreen /> } />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </React.StrictMode>
 );
